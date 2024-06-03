effb58bfd0e898f41c6e3723013220fbd19922eb
unknown <developSungHyun@gmail.com>
2024-06-03 14:37:11 +0900

    라이다센서 데이터 가져오는 코드 추가

--- Arduino/MainController/MainController.ino
+++ Arduino/MainController/MainController.ino
@@ -1,22 +1,15 @@
 #include <Wire.h>
 #include "Arduino.h"
-#include "Segment_4ch.h"
 #include "GpsData.h"
 #include "Moter.h"
 #include "BatteryLevel.h"
 
-Segment_4ch segment_4ch;
 GpsData gps_data;
 Moter moter;
 BatteryLevel battery_level;
 //GPS센서 통신핀
-#define GPS_SDA 26  // GPIO 9번을 SDA로 설정
-#define GPS_SCL 27  // GPIO 8번을 SCL로 설정
-
-//시프트레지스터를 통해 7세그먼트 제어핀
-#define dataPin 14   // DS Pin on 74HC595
-#define latchPin 12  // ST_CP Pin on 74HC595
-#define clockPin 13  // SH_CP Pin on 74HC595
+#define GPS_SDA 12  // GPIO 9번을 SDA로 설정
+#define GPS_SCL 13  // GPIO 8번을 SCL로 설정
 
 //왼쪽 모터
 #define MOTER_R_A 15
@@ -28,48 +21,110 @@ BatteryLevel battery_level;
 
 #define BATTERY_LEVEL_PIN 27
 
+int LiDAR_distance[3];
+
 double Latitude;   //위도값 저장
 double Longitude;  //경도값 저장
 
-int Moter_1_speed = 500, Moter_2_speed = 500;  //모터 속도
-bool MoterRotation_1 = true;       //true = 정회전, false = 역회전
-bool MoterRotation_2 = true;       //true = 회전, false = 역회전
+int Ste_Moter_1_speed = 0, Ste_Moter_2_speed = 0;  //설정한 모터 속도
+bool MoterRotation_1 = true;                       //true = 정회전, false = 역회전
+bool MoterRotation_2 = true;                       //true = 회전, false = 역회전
 
 long RunTime_Gps;  //GPS데이터를 주기적으로 가져오는 시간을 계산하기 위한 변수
 
 int GPS_GetTime = 5000;  //ms
 
+int L_moter_speed = 0;  //실제 속도
+int R_moter_speed = 0;  //실제 속도
+
+long L_moter_speed_timer;
+long R_moter_speed_timer;
+
 void setup() {
-  segment_4ch.Init(dataPin, latchPin, clockPin);  //7세그먼트 출력 준비
+
+  Wire.begin(8);                 // I2C 통신 Slave로 시작, 주소는 8
+  Wire.onReceive(receiveEvent);  // 수신 이벤트 등록
+
   gps_data.Init(GPS_SDA, GPS_SCL);                //GPS 데이터 수신 준비
-  Serial.begin(9600);                             //시리얼 출력 준비
+  Serial.begin(115200);                             //시리얼 출력 준비
   moter.Init(MOTER_R_A, MOTER_R_B, MOTER_L_A, MOTER_L_B);
   battery_level.Init(BATTERY_LEVEL_PIN);
-analogReadResolution(12);
+  analogReadResolution(12);
   //Wire.begin(25, 33);  // SDA, SCL 핀 지정(현재는 사용 X)
 
   RunTime_Gps = millis();  //현재 RunTime 저장
+
+  L_moter_speed_timer = millis();
+  R_moter_speed_timer = millis();
 }
 
 void loop() {
-  moter.Moter_1(MoterRotation_1, Moter_1_speed);  //모터1 제어
-  moter.Moter_2(MoterRotation_2, Moter_2_speed);  //모터2 제어
-
-battery_level.UpDate();
-segment_4ch.print((int)(battery_level.Get_Voltage() * 100));  //7세그먼트에 배터리 전압 출력 전송 테스트값16.24
-
-  if (millis() - RunTime_Gps >= GPS_GetTime) {  //GPS_GetTime 변수 값 시간동안 주기적으로 실행
-    gps_data.UpDate();
-    Latitude = gps_data.GetLatitude();    //위도값 가져오기
-    Longitude = gps_data.GetLongitude();  //경도값 가져오기
-
-    //출력 테스트
-    Serial.print(Latitude, 8);
-    Serial.print(", ");
-    Serial.println(Longitude, 8);
-
-    RunTime_Gps = millis();
+  Serial.print(LiDAR_distance[0]);
+  Serial.print(", ");
+  Serial.print(LiDAR_distance[1]);
+  Serial.print(", ");
+  Serial.println(LiDAR_distance[2]);
+  
+  if (L_moter_speed <= Ste_Moter_1_speed) {
+    if (millis() - L_moter_speed_timer >= 20) {
+      L_moter_speed_timer = millis();
+      L_moter_speed++;
+      moter.Moter_1(MoterRotation_1, L_moter_speed);
+    }
+  } else {
+    L_moter_speed = Ste_Moter_1_speed;
+    moter.Moter_1(MoterRotation_1, L_moter_speed);
   }
 
+  if (R_moter_speed <= Ste_Moter_2_speed) {
+    if (millis() - R_moter_speed_timer >= 20) {
+      R_moter_speed_timer = millis();
+      R_moter_speed++;
+      moter.Moter_2(MoterRotation_2, R_moter_speed);
+    }
+  } else {
+    R_moter_speed = Ste_Moter_2_speed;
+    moter.Moter_2(MoterRotation_2, R_moter_speed);
+  }
+
+  battery_level.UpDate();
+
+  //   if (millis() - RunTime_Gps >= GPS_GetTime) {  //GPS_GetTime 변수 값 시간동안 주기적으로 실행
+  //     gps_data.UpDate();
+  //     Latitude = gps_data.GetLatitude();    //위도값 가져오기
+  //     Longitude = gps_data.GetLongitude();  //경도값 가져오기
+
+  //     //출력 테스트
+  //     Serial.print(Latitude, 8);
+  //     Serial.print(", ");
+  //     Serial.println(Longitude, 8);
+
+  //     RunTime_Gps = millis();
+  //   }
+
   //delay(100);
+}
+
+void receiveEvent(int bytes) {  //인터럽트로 라이다센서 데이터 받
+  String receivedData = "";
+  while (Wire.available()) {
+    char c = Wire.read();
+    receivedData += c;
+  }
+
+  // 받은 데이터를 파싱하여 출력
+  int index = receivedData.indexOf(':');
+  if (index != -1) {
+    String label = receivedData.substring(0, index);
+    String valueStr = receivedData.substring(index + 1);
+    int value = valueStr.toInt();  // 문자열을 정수로 변환
+    
+    if(label == "L"){
+      LiDAR_distance[0] = value;
+    }else if(label == "F"){
+      LiDAR_distance[1] = value;
+    }else if(label == "R"){
+      LiDAR_distance[2] = value;
+    }
+  }
 }
---
+++ Arduino/SubController/SubController.ino
@@ -0,0 +1,113 @@
+#include <Wire.h>
+#include <RPLidar.h>
+
+#define RPLIDAR_MOTOR 3
+
+RPLidar lidar;
+
+const float distanceThreshold = 2000;
+const float frontAngleMin = 325;
+const float frontAngleMax = 35;
+const float leftAngleMin = 290;
+const float leftAngleMax = 325;
+const float rightAngleMin = 35;
+const float rightAngleMax = 70;
+
+String dataString = ""; // 전송할 데이터를 저장할 문자열 변수
+
+
+class LowPassFilter {
+public:
+    LowPassFilter(float alpha) : alpha(alpha), filteredValue(0) {}
+
+    float filter(float newValue) {
+        filteredValue = alpha * newValue + (1 - alpha) * filteredValue;
+        return filteredValue;
+    }
+
+private:
+    float alpha; // 필터 계수 (0과 1 사이의 값)
+    float filteredValue; // 이전 필터 값을 저장
+};
+
+LowPassFilter filter1(0.05); // 필터 객체 생성, alpha 값은 0.1로 설정
+LowPassFilter filter2(0.05); // 필터 객체 생성, alpha 값은 0.1로 설정
+LowPassFilter filter3(0.05); // 필터 객체 생성, alpha 값은 0.1로 설정
+
+
+void setup() {
+  Wire.begin(); // I2C 통신을 마스터로 초기화
+  lidar.begin(Serial1);
+  Serial.begin(115200);
+  pinMode(RPLIDAR_MOTOR, OUTPUT);
+  startMotor();
+  lidar.startScan();
+}
+
+void loop() {
+  dataString = ""; // 데이터 문자열 초기화
+
+  if (IS_OK(lidar.waitPoint())) {
+    float distance = lidar.getCurrentPoint().distance;
+    float angle = lidar.getCurrentPoint().angle;
+
+    if (distance > 0 && distance < distanceThreshold) {
+      if (isInLeftRange(angle)) {
+        dataString += "L:";
+        dataString += String((int)filter1.filter(distance));
+        dataString += ",";
+        Serial.println(dataString);
+      }
+
+      if (isInFrontRange(angle)) {
+        dataString += "F:";
+        dataString += String((int)filter2.filter(distance));
+        dataString += ",";
+        //Serial.println(dataString);
+      }
+
+      if (isInRightRange(angle)) {
+        dataString += "R:";
+        dataString += String((int)filter3.filter(distance));
+        dataString += ",";
+        //Serial.println(dataString);
+      }
+    }
+  } else {
+    stopMotor();
+    rplidar_response_device_info_t info;
+    if (IS_OK(lidar.getDeviceInfo(info, 100))) {
+       lidar.startScan();
+       startMotor();
+       delay(1000);
+    }
+  }
+
+  // 데이터 문자열을 I2C 통신으로 전송
+  Wire.beginTransmission(8); // 장치 주소 8로 전송
+  Wire.write(dataString.c_str()); // 문자열을 전송
+  Wire.endTransmission();
+
+  //delay(100); // 필요에 따라 지연 시간 조절
+}
+
+bool isInFrontRange(float angle) {
+  return (angle >= frontAngleMin || angle <= frontAngleMax);
+}
+
+bool isInLeftRange(float angle) {
+  return (angle >= leftAngleMin && angle <= leftAngleMax);
+}
+
+bool isInRightRange(float angle) {
+  return (angle >= rightAngleMin && angle <= rightAngleMax);
+}
+
+void startMotor() {
+  analogWrite(RPLIDAR_MOTOR, 255);
+}
+
+void stopMotor() {
+  analogWrite(RPLIDAR_MOTOR, 0);
+}
+
